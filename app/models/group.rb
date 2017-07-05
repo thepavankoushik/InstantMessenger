0a66b20b5a803389370fafee0a7975e55fe8adab
pavankoushik22 <pavankoushik22@gmail.com>
2017-07-05 12:57:30 +0530

    validations adjustment

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ActiveRecord::Base
+	validates :name, presence: true
+	validates :members, length: {minimum: 1 , maximum: 5}
+	validates :user, presence: true
 	belongs_to :user
 	has_many :group_messages
 	
--- app/models/group_message.rb
+++ app/models/group_message.rb
@@ -1,4 +1,5 @@
 class GroupMessage < ActiveRecord::Base
+	validates :message,presence: true
 	belongs_to :group
 	belongs_to :sender, :class_name => 'User'
 end
